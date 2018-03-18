126b1965baebeba7372528f5dcb6fa9de8f095f0
SanchezChristopher <32974560+SanchezChristopher@users.noreply.github.com>
2018-03-17 22:58:55 -0700

    Fancy Music settings
    Bass God Unity Guy:
    https://unity3d.com/learn/tutorials/topics/audio/adding-music-your-game
    
    Added unity standard assests to gitignore. Would prob takes up alot of repo space.

---
+++ Con-Jet-Re-Op/Assets/_Scripts/musicControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class musicControl : MonoBehaviour {
+
+    public AudioMixerSnapshot outOfCombat;
+    public AudioMixerSnapshot inCombat;
+    public AudioClip[] stings;
+    public AudioSource stingSource;
+
+    //will make the fade better if its in time with the music
+    public float bpm  = 128;
+    private float m_TransitionIn;
+    private float m_TransitionOut;
+    private float m_QuarterNote;
+
+	void Start () {
+        m_QuarterNote = 60 / bpm;
+        m_TransitionIn = m_QuarterNote;
+        m_TransitionOut = m_QuarterNote * 32;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CombatZone"))
+        {
+            inCombat.TransitionTo(m_TransitionIn);
+            PlaySting();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("CombatZone"))
+        {
+            outOfCombat.TransitionTo(m_TransitionOut);
+        }
+    }
+
+    void PlaySting()
+    {
+        int randClip = Random.Range(0, stings.Length);
+        stingSource.clip = stings[randClip];
+        stingSource.Play();
+    }
+}

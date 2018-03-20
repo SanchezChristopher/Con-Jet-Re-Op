e48c8d5c63883736b95afb6fdc2f5e30d435e1bf
SanchezChristopher <32974560+SanchezChristopher@users.noreply.github.com>
2018-03-19 21:32:39 -0700

    Missile bro
    Bass God Brackattack:
    https://www.youtube.com/watch?v=0v_H3oOR0aU

---
+++ Con-Jet-Re-Op/Assets/_Scripts/homingMissile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class homingMissile : MonoBehaviour {
+
+    public Transform target;
+
+    public float speed = 5f;
+    public float rotateSpeed = 200f;
+    public GameObject explosionEffect;
+
+    private Rigidbody rb;
+
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        //target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	void FixedUpdate () {
+        //Cross Product
+        Vector3 direction = target.position - rb.position;
+
+        direction.Normalize();
+
+        Vector3 rotateAmount = Vector3.Cross(direction, transform.up);
+
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+
+        rb.velocity = transform.up * speed;
+	}
+
+    void OnTriggerEnter()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
